563dcb3950084c51409837475538bb84a276148e
AZS-99 <saher.zachary.adam@icloud.com>
2019-03-10 01:39:59 -0500

    Adding sum and numberOfDigits functions

--- MyJava/src/lang/Math.java
+++ MyJava/src/lang/Math.java
@@ -1,7 +1,33 @@
 package lang;
 
 public class Math {
+    /**
+     * @param x any double
+     * @return An approximated integer value for the passed double.
+     */
     public static int approximate(double x) {
         return (x + 0.5 >= (int) x + 1? (int) x + 1 : (int) x);
+    }
+
+
+    /**
+     * @param n Any positive value
+     * @return The summation of numbers from 0 till n
+     */
+    public static int sum (int n) {
+        if (n <= 1)
+            return n;
+        return sum(n - 1) + n;
+    }
+
+
+    /**
+     * @param number a positive number
+     * @return The number of digits of the passed number. For instance, passing 3450 returns 4
+     */
+    public static int numberOfDigits(int number) {
+        if (number < 10)
+            return 1;
+        return numberOfDigits(number/10) + 1;
     }
 }
--- MyJava/src/main/Main.java
+++ MyJava/src/main/Main.java
@@ -6,5 +6,11 @@ import static lang.Math.*;
 public class Main {
     public static void main(String[] args) {
         int x = approximate(10.5);
+        System.out.println(x);
+
+        int sumTill100 = sum(100);
+        System.out.println(sumTill100);
+
+        System.out.println(numberOfDigits(1234599));
     }
 }
